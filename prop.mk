0810341499f69cd4db0bf0abcd5bf64208dfe18f
GuilianoS <guiliano-sfalcin@hotmail.com>
2022-01-09 01:44:39 -0300

    A001D: tweaks for zram and lmkd

--- prop.mk
+++ prop.mk
@@ -247,27 +247,19 @@ persist.device_config.runtime_native.usap_pool_enabled=true
 
 # LMKD
 PRODUCT_PROPERTY_OVERRIDES += \
+ro.lmk.log_stats=true \
 ro.lmk.low=1001 \
-ro.lmk.medium=0 \
+ro.lmk.medium=800 \
 ro.lmk.critical=0 \
 ro.lmk.critical_upgrade=false \
 ro.lmk.upgrade_pressure=100 \
 ro.lmk.downgrade_pressure=100 \
 ro.lmk.kill_heaviest_task=true \
 ro.lmk.kill_timeout_ms=100 \
-ro.lmk.use_minfree_levels=true \
-ro.lmk.log_stats=true
+ro.lmk.use_minfree_levels=true
 
-#HWUI properties
+# ZRAM
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.hwui.texture_cache_size=72 \
-ro.hwui.layer_cache_size=48 \
-ro.hwui.r_buffer_cache_size=8 \
-ro.hwui.path_cache_size=32 \
-ro.hwui.gradient_cache_size=1 \
-ro.hwui.drop_shadow_cache_size=6 \
-ro.hwui.texture_cache_flushrate=0.4 \
-ro.hwui.text_small_cache_width=1024 \
-ro.hwui.text_small_cache_height=1024 \
-ro.hwui.text_large_cache_width=2048 \
-ro.hwui.text_large_cache_height=1024
+ro.zram.mark_idle_delay_mins=60 \
+ro.zram.first_wb_delay_mins=180 \
+ro.zram.periodic_wb_delay_hours=24
