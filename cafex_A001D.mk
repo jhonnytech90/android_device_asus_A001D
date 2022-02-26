7c1bf1f0fb73361a441517f5fae783f6162d3c83
jhonatan Mikael <75139386+jhonnytech90@users.noreply.github.com>
2022-02-26 12:03:25 -0300

    update to cafex

--- cafex_A001D.mk
+++ cafex_A001D.mk
@@ -26,7 +26,7 @@ $(call inherit-product, vendor/dot/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := A001D
-PRODUCT_NAME := dot_A001D
+PRODUCT_NAME := cafex_A001D
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_A001D
 PRODUCT_MANUFACTURER := asus
